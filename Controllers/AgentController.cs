3da625d72f8c5c2fae6ded9efabc23fc8cc1f2ac
Jon <jsmartware@aol.com>
2021-04-04 11:37:05 -0700

    Added AgentController, only accessible by Agents not Admins

---
+++ Controllers/AgentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace FBI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize(Policy = "Agent")]
+    public class AgentController : Controller
+    {
+        public AgentController()
+        {
+
+        }
+    }
+}
