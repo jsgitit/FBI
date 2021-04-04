a03b4ee3b14aff2284603d7d5ff8d04ebe18afce
Jon <jsmartware@aol.com>
2021-04-03 21:01:04 -0700

    Added AdminController and solution file

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : ControllerBase
+    {
+        public AdminController()
+        {
+
+        }
+    }
+}
